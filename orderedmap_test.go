82392f2f89fe35bf477b0b1728fab8bb0205ced7
Meno Abels <meno.abels@adviser.com>
2023-09-26 14:41:00 +0200

    Fixed enable the mutation of a unmarshaled json.       In the decode methods the nested orderedMaps had       not been pointer with prevents the mutation.       To the fix broke some tests the cast to x.(OrderedMap)       failed. To Fix this the OrderedMap is now an interface.       This broke the TestUnmarshalJSONStruct test to fix       the export of OrderedMapImpl is need. This change       is a breaking!

--- orderedmap_test.go
+++ orderedmap_test.go
@@ -69,10 +69,10 @@ func TestOrderedMap(t *testing.T) {
 	// Values method
 	values := o.Values()
 	expectedValues := map[string]interface{}{
-		"number": 4,
-		"string": "x",
+		"number":  4,
+		"string":  "x",
 		"strings": []string{"t", "u"},
-		"mixed": []interface{}{ 1, "1" },
+		"mixed":   []interface{}{1, "1"},
 	}
 	if !reflect.DeepEqual(values, expectedValues) {
 		t.Error("Values method returned unexpected map")
@@ -493,7 +493,7 @@ func TestUnmarshalJSONArrayOfMaps(t *testing.T) {
 
 func TestUnmarshalJSONStruct(t *testing.T) {
 	var v struct {
-		Data *OrderedMap `json:"data"`
+		Data OrderedMapImpl `json:"data"`
 	}
 
 	err := json.Unmarshal([]byte(`{ "data": { "x": 1 } }`), &v)
@@ -593,5 +593,49 @@ func TestOrderedMap_empty_map(t *testing.T) {
 		t.Error("Empty map does not serialise to json correctly")
 		t.Error("Expect", srcStr)
 		t.Error("Got", marshalledStr)
+	}
+}
+
+func TestMutableAfterUnmarshal(t *testing.T) {
+	const input = `{
+	"foo": "bar",
+	"prop": {
+		"v1": 1,
+		"v2": "v1"
+	}
+}`
+	out := New()
+	err := json.Unmarshal([]byte(input), &out)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	iout, _ := out.Get("prop")
+	iout.(OrderedMap).Set("v3", "blabla")
+
+	if out.Keys()[0] != "foo" {
+		t.Fatal("key order changed")
+	}
+	if out.Keys()[1] != "prop" {
+		t.Fatal("key order changed")
+	}
+	inPropX, _ := out.Get("prop")
+	inProp := inPropX.(OrderedMap)
+
+	v1, _ := inProp.Get("v1")
+	if inProp.Keys()[0] != "v1" || v1.(float64) != 1 {
+		t.Fatal("key order changed")
+	}
+	v2, _ := inProp.Get("v2")
+	if inProp.Keys()[1] != "v2" || v2.(string) != "v1" {
+		t.Fatal("key order changed")
+	}
+
+	if inProp.Keys()[2] != "v3" {
+		t.Fatal("key order changed")
+	}
+	v3, _ := inProp.Get("v3")
+	if v3.(string) != "blabla" {
+		t.Fatal("expect blabla")
 	}
 }
